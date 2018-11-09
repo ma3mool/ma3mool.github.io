7501221ef3bb1afddd7936620ae01e2c6c77e15d
Abdulrahman Mahmoud <abdulrahman.h.mahmoud@gmail.com>
2018-11-08 19:58:19 -0600

    update

---
+++ _publications/minotaur19.md
@@ -0,0 +1,13 @@
+---
+title: "Minotaur: Adapting Software Testing Techniques for Hardware Errors"
+authors: "<u>Abdulrahman Mahmoud</u>, Radha Venkatagiri, Khalique Ahmed, Sasa Misailovic, Darko Marinov, Christopher W. Fletcher, Sarita V. Adve"
+collection: publications
+permalink: /publication/minotaur2019
+date: 2019-04-17
+venue: 'The 24th International Conference on Architectural Support for Programming Languages and Operating Systems' 
+venue_type: conference
+conf_shorthand: 'ASPLOS'
+location: Providence, Rhode Island 
+acceptance_rate: 21.1%
+other: (To appear in ASPLOS' 19) 
+---
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,32 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
+    "Princeton NJ, USA", 
+    40.3492744, 
+    -74.6592958
+  ], 
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
+    "Austin, TX, USA", 
+    30.2711286, 
+    -97.7436995
+  ], 
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
+    "Berkeley CA, USA", 
+    37.8708393, 
+    -122.2728639
+  ], 
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "San Francisco, California", 
+    37.7647993, 
+    -122.4629897
+  ], 
+  [
+    "London, UK", 
+    51.5073219, 
+    -0.1276474
+  ], 
+  [
+    "Los Angeles, CA", 
+    34.0536834, 
+    -118.2427669
   ]
 ];
